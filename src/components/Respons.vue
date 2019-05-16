a435ea6273da3f4c95aa8fb3db6b2ba38ef09e6a
vlad17451@gmail.com <pentium451>
2019-05-16 15:53:03 +0700

    btn

--- src/components/Respons.vue
+++ src/components/Respons.vue
@@ -4,7 +4,31 @@
       <div class="mb-3">{{label.text}}</div>
       <button class="btn-resp" v-if="label.step === 1">Взять кредит</button>
       <button class="btn-resp" v-if="label.step === 1">Пополнить счет</button>
-      <div>12123</div>
+      <div>
+        <div class="row resp-table">
+          <div class="col-3">Оплата услуг</div>
+          <div class="col-2">Счет списания</div>
+          <div class="col-2">Получатель</div>
+          <div class="col-1 ">Дата</div>
+          <div class="col-2">Сумма</div>
+          <div class="col-2">Кешбек</div>
+        </div>
+        <div class="dev-line mt-3"></div>
+        <div class="row resp-table">
+          <div class="col-3">Купил покушать</div>
+          <div class="col-2">
+            <div>Visa</div>
+            <div>**** 1337</div>
+          </div>
+          <div class="col-2">Пятерочка</div>
+          <div class="col-1 text-center">
+            <div>Сегодня</div>
+            <div>18:00</div>
+          </div>
+          <div class="col-2">1 999,00 руб.</div>
+          <div class="col-2">Кешбек</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +42,13 @@
   }
 </script>
 <style lang="scss">
+  .dev-line {
+    border-bottom: 2px solid #dfdfdf;
+  }
+  .resp-table {
+    font-size: 16px;
+    width: 1000px;
+  }
   .btn-resp {
     font-size: 16px;
     background: #266dc2;
@@ -30,7 +61,7 @@
     }
   }
   .resp_box {
-    width: 700px;
+    min-width: 700px;
     border: solid 2px #ccddec;
     padding: 16px 30px;
     margin-bottom: 30px;
