3c2ae5afc1d0e02da2955de5115a2a6ad21bc728
gGerz <34414584+gGerz@users.noreply.github.com>
2019-05-16 10:47:16 +0700

    #lets roll

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,9 @@
           <div class="steps">
             <div class="step step__first" v-if="firstStep">
               <h1>Голосой помощник SmartWorld</h1>
-              <img class="micro" src="./assets/micro.png" alt="micro">
-              <div class="form-group w-100">
-                <label for="exampleInputEmail1">Что вы хотите сделать?</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Скажите что-нибудь...">
+                <img class="micro" @click="nextSecondStep()" src="./assets/micro.png" alt="micro">
               </div>
             </div>
             <div class="step step__second" v-if="secondStep">
@@ -19,7 +18,7 @@
           </div>
         </div>
       </div>
-      <div class="row justify-content-around">
+      <div class="row justify-content-around" v-if="!firstStep">
         <button class="btn btn-cancel">Отмена</button>
         <button class="btn btn-next" @click="nextSecondStep()">Спросить еще</button>
       </div>
@@ -70,6 +69,26 @@
 
     h1{
       margin: 20px 0;
+    }
+  }
+
+  .step__first{
+
+    .form-group{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+
+      input{
+        max-width: 250px;
+      }
+
+      img{
+        width: 50px;
+        height: 50px;
+        margin-left: 15px;
+      }
     }
   }
 
