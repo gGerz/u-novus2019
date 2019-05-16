93120b19b03457c3bba71b0d7690bd3449dfdf79
vlad17451@gmail.com <pentium451>
2019-05-16 17:49:46 +0700

    set

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,36 +15,36 @@
           </div>
         </div>
       </div>
-      <div class="card_line my-4"></div>
-      <div class="balance d-flex justify-content-between">
-        <div class="">
-          <div class="card_title">Visa classic<img class="visa ml-3" src="../assets/visa.jpg" alt=""></div>
-          <div class="font14">
-            Дебютовая карта
-          </div>
-          <div class="font14">**** 0451, дествительно до 11/2020</div>
-        </div>
-        <div class="d-flex">
-          <div class="font24">
-             419 420.69 Р
-          </div>
-        </div>
-      </div>
-      <div class="card_line my-4"></div>
-      <div class="balance d-flex justify-content-between">
-        <div class="">
-          <div class="card_title">Visa classic<img class="visa ml-3" src="../assets/visa.jpg" alt=""></div>
-          <div class="font14">
-            Кредитная карта
-          </div>
-          <div class="font14">**** 3822, дествительно до 11/2020</div>
-        </div>
-        <div class="d-flex">
-          <div class="font24">
-            288 000.69 Р
-          </div>
-        </div>
-      </div>
+<!--      <div class="card_line my-4"></div>-->
+<!--      <div class="balance d-flex justify-content-between">-->
+<!--        <div class="">-->
+<!--          <div class="card_title">Visa classic<img class="visa ml-3" src="../assets/visa.jpg" alt=""></div>-->
+<!--          <div class="font14">-->
+<!--            Кредитные карта-->
+<!--          </div>-->
+<!--          <div class="font14">**** 0451, дествительно до 11/2020</div>-->
+<!--        </div>-->
+<!--        <div class="d-flex">-->
+<!--          <div class="font24">-->
+<!--             419 420.69 Р-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="card_line my-4"></div>-->
+<!--      <div class="balance d-flex justify-content-between">-->
+<!--        <div class="">-->
+<!--          <div class="card_title">Visa classic<img class="visa ml-3" src="../assets/visa.jpg" alt=""></div>-->
+<!--          <div class="font14">-->
+<!--            Кредитная карта-->
+<!--          </div>-->
+<!--          <div class="font14">**** 3822, дествительно до 11/2020</div>-->
+<!--        </div>-->
+<!--        <div class="d-flex">-->
+<!--          <div class="font24">-->
+<!--            288 000.69 Р-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
